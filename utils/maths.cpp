337e0ae0bf036758c2eed81e707760a1486ad771
LuJu <lucas.julieron@gmail.com>
2014-04-14 10:18:24 -0400

    removed energy functions

--- utils/maths.cpp
+++ utils/maths.cpp
@@ -34,13 +34,7 @@ double deg2rad(double angle){
 }
 
 
-double kinetic_energy(double speed, double mass){
-    return (speed*speed*mass)/2;
-}
 
-double potential_energy(double mass, double gravitation, double height){
-    return (mass*gravitation*height);
-}
 
 double absolute_value(double value){
     return (value >= 0)?value : -value;
